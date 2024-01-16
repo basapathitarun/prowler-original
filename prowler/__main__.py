4680ed53c39e80fbcb2caf862432dcc838f7fbfd
basapathitarun <101045826+basapathitarun@users.noreply.github.com>
2024-01-16 18:26:00 +0530

    Update __main__.py

--- prowler/__main__.py
+++ prowler/__main__.py
@@ -72,14 +72,26 @@ def prowler():
     checks_folder = args.checks_folder
     severities = args.severity
     # compliance_framework = args.compliance
-    compliance_framework = ['cis_1.4_aws']
+    # compliance_framework = ['cis_1.4_aws']
     custom_checks_metadata_file = args.custom_checks_metadata_file
 
     
     print(f"args :{args}\n")
-    
 
-    
+    compliance = ['aws_audit_manager_control_tower_guardrails_aws', 'aws_foundational_security_best_practices_aws', 'aws_well_architected_framework_reliability_pillar_aws', 'aws_well_architected_framework_security_pillar_aws', 'cisa_aws', 'cis_1.4_aws', 'cis_1.5_aws', 'cis_2.0_aws', 'ens_rd2022_aws', 'fedramp_low_revision_4_aws', 'fedramp_moderate_revision_4_aws', 'ffiec_aws', 'gdpr_aws', 'gxp_21_cfr_part_11_aws', 'gxp_eu_annex_11_aws', 'hipaa_aws', 'iso27001_2013_aws', 'mitre_attack_aws', 'nist_800_171_revision_2_aws', 'nist_800_53_revision_4_aws', 'nist_800_53_revision_5_aws', 'nist_csf_1.1_aws', 'pci_3.2.1_aws', 'rbi_cyber_security_framework_aws', 'soc2_aws', 'cis_2.0_gcp']
+
+    dict_compliance = {}
+    i=1
+    for each in compliance:
+        dict_compliance[i]=each
+        i=i+1
+
+    for key,value in dict_compliance.items():
+        print(f"{key} for {value}")
+
+    ans = int(input("Enter which compliance to scan\n"))
+
+    compliance_framework = [dict_compliance[ans]]
     if not args.no_banner:
         print_banner(args)
 
